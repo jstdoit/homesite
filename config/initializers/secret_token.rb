0b27186a6f1508130b96a27c68a4b7e57d086853
ganggang <just_send@163.com>
2012-05-15 12:11:44 +0800

    init the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homesite::Application.config.secret_token = 'acdfd83b6fab6b5343869133d15f35a05972e2c824c0319d162e4893670d1bd7ed3469d3ae4bdfb29ba4324b20746a33cadbbe6789134a35444c838000a95151'
